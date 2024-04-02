ef3cde56103b8e0f563e08ece74e3bdec49af7fe
Dmytro Denysov <dmytro.denysov@danads.se>
2024-04-02 10:37:44 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Button from 'primevue/button';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>ololo</div>
+  <Button label="Submit" />
 </template>
 
 <style scoped>
---
+++ src/components/button/UiButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+</script>
+
+<template>
+  <Button label="Hello Vitest" />
+</template>
+
+<style scoped>
+
+</style>
