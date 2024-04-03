e0d0cf1f49750ed95af1fe4871c671cdb4d9515d
Dmytro Denysov <dmytro.denysov@danads.se>
2024-04-03 11:23:27 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,72 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
+import Button from 'primevue/button'
+import Toolbar from 'primevue/toolbar'
+import Menubar from 'primevue/menubar';
+import Avatar from 'primevue/avatar';
+
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home'
+  },
+  {
+    label: 'Features',
+    icon: 'pi pi-star'
+  },
+  {
+    label: 'Projects',
+    icon: 'pi pi-search',
+    items: [
+      {
+        label: 'Components',
+        icon: 'pi pi-bolt'
+      },
+      {
+        label: 'Blocks',
+        icon: 'pi pi-server'
+      },
+      {
+        label: 'UI Kit',
+        icon: 'pi pi-pencil'
+      },
+      {
+        label: 'Templates',
+        icon: 'pi pi-palette',
+        items: [
+          {
+            label: 'Apollo',
+            icon: 'pi pi-palette'
+          },
+          {
+            label: 'Ultima',
+            icon: 'pi pi-palette'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'Contact',
+    icon: 'pi pi-envelope'
+  }
+]);
 </script>
 
 <template>
-  <div>ololo</div>
-  <Button label="Submit" />
+  <Toolbar class="border-noround surface-overlay py-3 px-6 shadow-2 flex align-items-center justify-content-between relative lg:static">
+    <template #start>
+      <div class="flex align-items-center gap-2">
+        <img src="https://blocks.primevue.org/images/blocks/logos/bastion-700.svg" />
+        <Menubar class="border-none" :model="items" />
+      </div>
+    </template>
+
+    <template #end>
+      <Avatar class="p-overlay-badge" icon="pi pi-user" shape="circle" size="medium" />
+    </template>
+  </Toolbar>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primeicons/primeicons.css'
+import '/node_modules/primeflex/primeflex.css';
+import './assets/scss/main.scss';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
+import BadgeDirective from 'primevue/badgedirective';
 
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(PrimeVue)
+app.directive('badge', BadgeDirective);
 
 app.mount('#app')
