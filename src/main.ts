9e86b7d9142e2765b80272fcb6df93f90aff5267
Dmytro Denysov <dmytro.denysov@danads.se>
2024-05-08 12:44:19 +0300

    Reorganized code structure

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import 'primevue/resources/themes/aura-light-green/theme.css'
 import 'primeicons/primeicons.css'
 import '/node_modules/primeflex/primeflex.css';
-import '@/app/main/assets/scss/main.scss';
+import '@/app/default/assets/scss/main.scss';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './app/main/ui/App.vue'
+import App from '@/app/default/ui/App.vue'
 import PrimeVue from 'primevue/config';
 import BadgeDirective from 'primevue/badgedirective';
 
